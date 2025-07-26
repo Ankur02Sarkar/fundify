33482362fc5ef8e4d81b794a25107bd5e4fddf56
Ankur Sarkar <sarkarrohonankur@gmail.com>
2025-07-26 13:30:33 +0530

    refactor: remove unused bento grid component and its imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
-import BentoGridComp from "@/components/bento";
 import { Features } from "@/components/blocks/features-2";
 import { Faqs } from "@/components/faqs-4";
 import FooterSection from "@/components/footer";
 import HeroSection from "@/components/hero-section";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <Features />
-      <BentoGridComp />
       <Faqs />
       <FooterSection />
     </div>
